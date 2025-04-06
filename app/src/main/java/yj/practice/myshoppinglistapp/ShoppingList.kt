f1201d08314a5abb8d8aaec91523a3cbef060f94
YujeongLEE <dldbwjd24@gmail.com>
2025-04-06 13:03:16 +0900

    feat : shopping item list - edit/delete

--- app/src/main/java/yj/practice/myshoppinglistapp/ShoppingList.kt
+++ app/src/main/java/yj/practice/myshoppinglistapp/ShoppingList.kt
@@ -1,6 +1,7 @@
 package yj.practice.myshoppinglistapp
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,9 +9,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
@@ -36,7 +39,7 @@ data class ShoppingItem(
     val id: Int,
     var name: String,
     var quantity: Int = 0,
-    var useEditing: Boolean = false // default value, if not entered automatically set as "false"
+    var isEditing: Boolean = false // default value, if not entered automatically set as "false"
 )
 
 @Composable
@@ -64,45 +67,72 @@ fun ShoppingListApp() {
                 .padding(16.dp)
         ) {
             items(sItems) {
-                ShoppingListItem(it, {}, {})
+//                ShoppingListItem(it, {}, {})
+                    item -> // it 대신 더 명확한 이름으로 사용하고 싶은 경우 명명가능
+                if (item.isEditing) {
+                    ShoppingItemEditor(item = item, onEditComplete = { editedName, editedQuantity ->
+                        // onEditComplete : (string,int) -> unit
+
+                        //edit complete 함수가 작동하면, isEditing은 false가 되어야함
+                        // original을 copy하되, isEditing은 false로
+                        sItems = sItems.map { it.copy(isEditing = false) }
+                        val editedItem = sItems.find { it.id == item.id }
+                        editedItem?.let { // could be null..
+                            it.name = editedName
+                            it.quantity = editedQuantity
+                        }
+                    })
+                } else {
+                    ShoppingListItem(
+                        item = item,
+                        onEditClick = {//edit icon click시 수행할 작업
+                            //map에서 retrieval 하는 it 과 click대상 item의 id가 같음 -> 해당 아이템의 isEditing = true
+                            sItems = sItems.map { it.copy(isEditing = it.id == item.id) }
+                        },
+                        onDeleteClick = {
+                            sItems = sItems - item //이게 된다고? 리스트에서 제거할때?
+                        }
+                    )
+                }
             }
         }
 
         if (showDialog) {
-            AlertDialog(onDismissRequest = { showDialog = false }, confirmButton = {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(8.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Button(onClick = {
+            AlertDialog(
+                onDismissRequest = { showDialog = false }, confirmButton = {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Button(onClick = {
 
-                        if (itemName.isNotBlank() && itemQuantity.isNotBlank()) {
-                            val newItem = ShoppingItem(
-                                id = sItems.size + 1,
-                                name = itemName,
-                                quantity = itemQuantity.toInt() //app crash발생가능
-                            )
+                            if (itemName.isNotBlank() && itemQuantity.isNotBlank()) {
+                                val newItem = ShoppingItem(
+                                    id = sItems.size + 1,
+                                    name = itemName,
+                                    quantity = itemQuantity.toInt() //app crash발생가능
+                                )
 
-                            sItems = sItems + newItem
+                                sItems = sItems + newItem
+                                showDialog = false
+                                itemName = ""
+                                itemQuantity = ""
+                            }
+
+                        }) {
+                            Text("Add")
+                        }
+                        Button(onClick = {
                             showDialog = false
                             itemName = ""
                             itemQuantity = ""
+                        }) {
+                            Text("Cancel")
                         }
-
-                    }) {
-                        Text("Add")
                     }
-                    Button(onClick = {
-                        showDialog = false
-                        itemName = ""
-                        itemQuantity = ""
-                    }) {
-                        Text("Cancel")
-                    }
-                }
-            },
+                },
                 title = { Text("Add Shopping Item") }, text = {
                     Column {
                         OutlinedTextField(
@@ -128,6 +158,49 @@ fun ShoppingListApp() {
 
     }
 }
+
+//edit added shopping item
+@Composable
+fun ShoppingItemEditor(item: ShoppingItem, onEditComplete: (String, Int) -> Unit) {
+    var editedName by remember { mutableStateOf(item.name) }
+    var editedQuantity by remember { mutableStateOf(item.quantity.toString()) }
+    var isEditing by remember { mutableStateOf(item.isEditing) }
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+            .padding(8.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    )
+    {
+        Column {
+            BasicTextField(
+                value = editedName,
+                onValueChange = { editedName = it },
+                singleLine = true, //(limit)user only enter single line
+                modifier = Modifier
+                    .wrapContentSize() //
+                    .padding(8.dp)
+            )
+            BasicTextField(
+                value = editedQuantity,
+                onValueChange = { editedQuantity = it },
+                singleLine = true, //(limit)user only enter single line
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(8.dp)
+            )
+        }
+        Button(onClick = {
+            isEditing = false
+            onEditComplete(editedName, editedQuantity.toIntOrNull() ?: 1) // default value = 1
+        }) {
+            Text("Save")
+        }
+    }
+}
+
 
 @Composable
 fun ShoppingListItem(
